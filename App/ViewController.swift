fc09623acef1a6bf752401d87f0b9516c47ffb49
Francesca Cascella <francescacascella@10-21-48-81.0200.sec.it.iosda.org>
2017-06-06 10:56:28 +0200

    inserisco imm

--- App/ViewController.swift
+++ App/ViewController.swift
@@ -9,17 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBOutlet weak var nome: UITextField!
+    
+    @IBOutlet weak var casella: UITextView!
+    
+    @IBOutlet var tastiera: UITapGestureRecognizer!
+  
+    
+    @IBOutlet var immag: UIImageView!
+    @IBAction func tap() {
+    immag.image = UIImage(named: "images")
+        
+        var inseriscinome: String = nome.text!
+        inseriscinome = inseriscinome.trimmingCharacters(in: CharacterSet.whitespaces).capitalized
+        
+        if (inseriscinome == "Luca" || inseriscinome == "Giuseppe" || inseriscinome == "Lelio" || inseriscinome == "Pasquale") {
+            casella.text = "Ciao \(inseriscinome), sei un istruttore fantastico!"
+        }
+        else {
+            casella.text = "Ciao \(inseriscinome) , ti auguro una buona giornata al corso IOS Foundation Program"
+        }
+        nome.resignFirstResponder()
+    }
 }
+
+
+
 
